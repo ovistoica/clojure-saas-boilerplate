df1fc9d34ee2e23be1fca96854049bb258088ec2
Ovi Stoica <ost@3e.eu>
2023-06-01 16:58:43 +0400

    Remove infra and circle ci

--- infra/cloud-watch.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_cloudwatch_log_group" "clojure_saas_app" {
-  name = "clojure_saas_app"
-}
-
-resource "aws_cloudwatch_log_group" "clojure_saas_db" {
-  name = "clojure_saas_db"
-}
--- infra/cluster.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_ecs_cluster" "clojure_saas_ecs_cluster" {
-  name = var.ecs_cluster
-}
